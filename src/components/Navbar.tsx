df5b4d6f9246c219cc2b91a20ea2c67cb6b330c1
manaher <manahir.irfan@gmail.com>
2024-11-10 20:35:34 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='container pt-8'>
         <div className='flex justify-between items-center'>
-            <div className='text-xl font-medium'>Manaher</div>
+            <div className='text-2xl font-medium'>Manaher</div>
             <ul className='gap-10 lg:gap-16 hidden md:flex'>
                 <li className='menuLink'><a href='#hero'>Home</a></li>
                 <li className='menuLink'><a href='#about'>About</a></li>
